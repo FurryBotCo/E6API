6a659d0778cc3fdfc268418e8335485f2be8b92e
Donovan_DMC <dwddaniels262@gmail.com>
2019-08-18 15:04:16 -0500

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,379 @@
+import phin from "phin";
+import util from "util";
+import pkg from "./package.json";
+import * as fs from "fs";
+import qs from "querystring";
+
+class APIError extends Error {
+	constructor(err: string, message: string, response?: any) {
+		response ? super(`${err}, ${message}, response: ${util.inspect(response, { depth: 1 })}`) : super(`${err}, ${message}`);
+		this.name = "APIError";
+	}
+}
+
+interface E621Post {
+	id: number;
+	tags: string;
+	locked_tags?: string;
+	description?: string;
+	created_at: {
+		json_class: "Time";
+		s: number;
+		n: number;
+	};
+	creator_id?: number;
+	author?: string;
+	change: number;
+	source?: string;
+	score: number;
+	fav_count: number;
+	md5?: string;
+	file_size?: number;
+	file_url?: string;
+	file_ext?: string;
+	preview_url?: string;
+	preview_width?: number;
+	preview_height?: number;
+	sample_url?: string;
+	sample_width?: number;
+	sample_height?: number;
+	rating: "s" | "q" | "e";
+	status: "active" | "deleted";
+	width: number;
+	height: number;
+	has_comments: boolean;
+	has_notes: boolean;
+	has_childeren?: boolean;
+	childeren?: string;
+	parent_id?: string;
+	artist?: string[];
+	sources?: string[];
+	delreason?: string;
+}
+
+interface E621Tag {
+	name: string;
+	id: number;
+	count: number;
+	type: 0 | 1 | 2 | 3 | 4 | 5;
+}
+
+class E6API {
+	apiKey: string;
+	userAgent: string;
+	constructor(options?: {
+		apiKey?: string;
+		userAgent?: string;
+	}) {
+		if (!options) options = {};
+		if (!options.apiKey) options.apiKey = "";
+		if (!options.userAgent) options.userAgent = `E6API/${pkg.version} (https://github.com/FurryBotCo/E6API)`;
+
+		this.apiKey = options.apiKey;
+		this.userAgent = options.userAgent;
+	}
+
+	async login(user: string, pass: string) {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/user/login.json?name=${user}&password=${pass}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+			return this.apiKey = b.password_hash;
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	// not implemented yet
+	/*async createPost(post: {
+		tags: string[];
+		file: fs.WriteStream;
+		upload_url?: string;
+		rating: "safe" | "questionable" | "explicit";
+		source: string[];
+		description?: string;
+		is_rating_locked?: boolean;
+		is_note_locked?: boolean;
+		parent_id?: number;
+	} | {
+		tags: string[];
+		file?: fs.WriteStream;
+		upload_url: string;
+		rating: "safe" | "questionable" | "explicit";
+		source: string[];
+		description?: string;
+		is_rating_locked?: boolean;
+		is_note_locked?: boolean;
+		parent_id?: number;
+	}): Promise<any> {
+	}*/
+
+	// not tested/finished yet
+	/*async updatePost(id: number, post: {
+		tags?: string[];
+		old_tags?: string[];
+		rating?: "safe" | "questionable" | "explicit";
+		source?: string[];
+		description?: string;
+		is_rating_locked?: boolean;
+		is_note_locked?: boolean;
+		parent_id?: number;
+	}, reason: string) {
+		return phin({
+			method: "POST",
+			url: "https://e621.net/post/update.json",
+			data: {
+				id,
+				post,
+				reason
+			},
+			parse: "json"
+		}).then(res => {
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body);
+			else return res.body;
+		}).catch(err => {
+			throw err;
+		});
+	}*/
+
+	getPostById(id: number, fetchImage?: false): Promise<E621Post>;
+	getPostById(id: number, fetchImage?: true): Promise<{ image: Buffer; post: E621Post }>;
+
+	async getPostById(id: number, fetchImage?: boolean) {
+		if (typeof fetchImage !== "boolean") fetchImage = false;
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/show.json?id=${id}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(async (res) => {
+			if (res.statusCode === 404) return null/*throw new APIError(`${res.statusCode} ${res.statusMessage}`, "invalid post id")*/;
+
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			if (fetchImage) return {
+				image: await phin(b.file_url).then(i => i.body),
+				post: b
+			};
+			else return b;
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	getPostByMD5(md5: string, fetchImage?: false): Promise<E621Post>;
+	getPostByMD5(md5: string, fetchImage?: true): Promise<{ image: Buffer; post: E621Post }>;
+
+	async getPostByMD5(md5: string, fetchImage?: boolean) {
+		if (typeof fetchImage !== "boolean") fetchImage = false;
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/check_md5.json?md5=${md5}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(async (res): Promise<any> => {
+			if (res.statusCode === 404) return null;
+
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			if (!b.exists) return null;
+
+			return fetchImage ? this.getPostById(b.post_id, fetchImage as true) : this.getPostById(b.post_id, fetchImage as false);
+		});
+	}
+
+	async getPostTagsById(id: number): Promise<{ id: number; tags: E621Tag[] }> {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/tags.json?id=${id}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode === 404) return null;
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			return {
+				id,
+				tags: b
+			};
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	async getPostTagsByMD5(md5: string): Promise<{ md5: string; tags: E621Tag[] }> {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/tags.json?md5=${md5}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode === 404) return null;
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			return {
+				md5,
+				tags: b
+			};
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	async listPosts(tags?: [string?, string?, string?, string?, string?, string?], limit?: number, page?: number, beforeId?: number, filterTags?: string[]) {
+		const q: {
+			tags?: string;
+			limit?: number;
+			page?: number;
+			before_id?: number;
+		} = {};
+
+		if (filterTags) filterTags = filterTags.map(t => t.startsWith("-") ? t.slice(1, t.length) : t);
+		if (tags && filterTags) if (tags.some(t => filterTags.map(t => t.startsWith("-") ? t.slice(1, t.length).toLowerCase() : t.toLowerCase()).includes(t.toLowerCase()))) throw new TypeError("'tags' conatins a tag that is listed in 'filterTags'");
+		if (tags && tags.length > 0) {
+			if (tags.length > 6) throw new TypeError("you cannot use more than 6 tags");
+			else q.tags = tags.join(" ");
+
+			if (filterTags && tags.some(t => filterTags.map(t => t.startsWith("-") ? t.slice(1, t.length).toLowerCase() : t.toLowerCase()).includes(t.toLowerCase()))) throw new TypeError("'tags' conatins a tag that is listed in 'filterTags'");
+		}
+		if (limit && limit > 0 && limit < 320) q.limit = limit;
+		if (page && page > 0 && page < 750) q.page = page;
+		if (beforeId) q.before_id = beforeId;
+
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/index.json${Object.keys(q).length > 0 ? `?${qs.encode(q)}` : ""}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode === 404) return null;
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			return filterTags && filterTags.length > 0 ? b.filter(p => !filterTags.some(t => p.tags.split(" ").includes(t))) : b;
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	// flagPost(id: number, flagOption: "inferior", inferiorParent: number): Promise<boolean>;
+	// flagPost(id: number, flagOption: "uploader" | "inferior" | 1 | 2 | 3 | 4 | 5 | 6): Promise<boolean>;
+
+	// untested
+	/*async flagPost(id: number, flagOption: "uploader" | "inferior" | 1 | 2 | 3 | 4 | 5 | 6, inferiorParent?) {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/flag.json?id=${id}&flag_option=${flagOption}${flagOption === "inferior" ? `&inferior_parent=${inferiorParent}` : ""}`,
+			headers: {
+				"Authorization": this.apiKey,
+				"User-Agent": this.userAgent
+			},
+			parse: "json"
+		}).then(res => {
+			if (res.statusCode === 404) return false;
+			else if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body);
+
+			return true;
+		}).catch(err => {
+			throw err;
+		});
+	}*/
+
+	// untested
+	/*async deletePost(id: number, reason: string, permanent: boolean): Promise<boolean> {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/delete.json?id=${id}&reason=${reason}`,
+			headers: {
+				"Authorization": this.apiKey,
+				"User-Agent": this.userAgent
+			},
+			parse: "json"
+		}).then(async (res) => {
+			if (res.statusCode === 404) return null;
+			else if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body);
+
+			if (permanent) return phin({
+				method: "GET",
+				url: `https://e621.net/post/delete.json?id=${id}&reason=${reason}&mode=1`,
+				headers: {
+					"Authorization": this.apiKey,
+					"User-Agent": this.userAgent
+				},
+				parse: "json"
+			}).then(res => {
+				if (res.statusCode === 404) return null;
+				else if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body);
+
+				return true;
+			});
+
+			return true;
+		}).catch(err => {
+			throw err;
+		});
+	}*/
+
+	async getDeletedPosts(page?: number, userId?: number): Promise<{ id: number; creator_id: number; author?: string; tags: string; delreason: string; }[]> {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/deleted_index.json${page || userId ? `?${page ? `page=${page}${userId ? `&user_id=${userId}` : ""}` : ""}` : ""}`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode === 404) return null;
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+
+			return b;
+		}).catch(err => {
+			throw err;
+		});
+	}
+
+	async getPopularPosts(type: "day" | "week" | "month"): Promise<E621Post[]> {
+		return phin({
+			method: "GET",
+			url: `https://e621.net/post/popular_by_${type}.json`,
+			headers: {
+				"User-Agent": this.userAgent
+			}
+		}).then(res => {
+			if (res.statusCode === 404) return null;
+			if (res.statusCode !== 200) throw new APIError(`${res.statusCode} ${res.statusMessage}`, res.body.toString());
+
+			const b = JSON.parse(res.body.toString());
+			return b;
+		}).then(err => {
+			throw err;
+		});
+	}
+
+	/*async revertPostTags(id: number, historyId: number) {
+
+	}*/
+}
+
+export = E6API;
---
+++ readme.md
@@ -0,0 +1,231 @@
+## <center>E621 Api Module</center>
+
+Currently, this module only supports fetching posts/post tags, the e621 api isn't very well documented, and it's hard to test things like creating posts, deleting posts, flagging posts, etc without getting barred from doing so, or just tossing in junk code, and hoping for the best. Some of the methods are sort of implemented, but they're commented out. I know I've done them wrong, which is why they are commented out. They were left there if I ever want to come back and try to solve them.
+
+### This module does not filter ratings! For an sfw filter, see [E9API](https://npmjs.org/package/e9api), that module will return ONLY sfw posts!
+
+This module also contains a `login` function to fetch your e621 api key, it currently isn't really useful, as the module doesn't use your api key, but if you need your e621 api key, you could use it for some reason. This will also set the current E6API instance's api key, but once again, it is not used.
+
+### !!IMPORTANT!! - You MUST set a valid userAgent when using this module, and you MUST not use any default library useragents, e621 blocks almost all of the default userAgents, and will completely block you if you try to send too many requests with a default agent. We are NOT responsible if you get blocked by E621!
+
+Example `login` function usage
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+e6.login("username", "password").then(res => console.log(res));
+// if successful, it will print your hashed password (api key) to the console
+```
+
+both of the `getPostBy` functions support fetching the image while getting the info for it, if set to true, it will return an object structure like `{ image: (ImageBuffer), post: (PostInfo) }`
+Example `getPostById` function usage, without fetching image
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//             post id, fetch image
+e6.getPostById(1022094, false).then(res => console.log(res));
+
+// if the post exists, this will log its info to the console, ex:
+/*
+{
+	id: 1022094,
+	tags: "2016 accessory alexis_velvet all_fours anthro balls_in_panties big_butt black_hair boy_shorts brown_fur bulge butt cervid clothed clothing fur furgonomics garter girly hair looking_at_viewer looking_back male mammal perineum presenting presenting_hindquarters rear_view smile solo spots tight_clothing underwear valkoinen",
+	locked_tags: null,
+	description: "Removed text tag",
+	created_at: {
+		json_class: "Time",
+		s: 1476493528,
+		n: 698320000
+	},
+	creator_id: 174059,
+	author: "GameManiac",
+	change: 21107428,
+	source: "http://www.furaffinity.net/view/21429634/",
+	score: 241,
+	fav_count: 855,
+	md5: "00304f67656e174b70c62160404464a7",
+	file_size: 1081587,
+	file_url: "https://static1.e621.net/data/00/30/00304f67656e174b70c62160404464a7.png",
+	file_ext: "png",
+	preview_url: "https://static1.e621.net/data/preview/00/30/00304f67656e174b70c62160404464a7.jpg",
+	preview_width: 150,
+	preview_height: 99,
+	sample_url: "https://static1.e621.net/data/sample/00/30/00304f67656e174b70c62160404464a7.jpg",
+	sample_width: 800,
+	sample_height: 533,
+	rating: "q",
+	status: "active",
+	width: 1280,
+	height: 853,
+	has_comments: true,
+	has_notes: false,
+	has_children: true,
+	children: "1022673,1022680,1029501,1097929,1109018",
+	parent_id: null,
+	artist: ["valkoinen"],
+	sources: ["http://www.furaffinity.net/view/21429634/",
+		"http://www.furaffinity.net/user/valkoinen/",
+		"http://d.facdn.net/art/valkoinen/1476493152/1476493152.valkoinen_clothed1280.png"
+	]
+}
+*/
+// if the post does not exist, it will return null
+```
+
+If `fetchImage` is set to `true`:
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//             post id, fetch image
+e6.getPostById(1022094, true).then(res => console.log(res));
+
+// if the post exists, this will log its info to the console, ex:
+/*
+{
+	image: (ImageBuffer),
+	post: (E621Post)
+}
+*/
+// if the post does not exist, it will return null
+```
+
+the `getImageByMD5` function follows the same principal
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//               post md5,                         fetch image
+e6.getPostByMD5("00304f67656e174b70c62160404464a7", false).then(res => console.log(res));
+
+// if the post exists, this will log its info to the console, ex:
+/*
+{
+	(E621Post)
+}
+*/
+// if the post does not exist, it will return null
+```
+
+You can also just fetch the tags of posts, if you only need the tags
+To fetch more about the post, you must use one of `getPostById` or `getPostByMD5`
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//                 post id
+e6.getPostTagsById(1022094).then(res => console.log(res));
+
+// if the post exists, it will return something like
+/*
+{
+	id: 1022094,
+  	tags: [{
+		id: 2153,
+       	name: "presenting",
+       	count: 84694,
+       	type: 0,
+		type_locked: null
+	}]
+}*/
+// tags will be an array of tags, with each having the same structure
+// if the post does not exist, it will return null
+```
+
+md5 example
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//                  post md5
+e6.getPostTagsByMD5("00304f67656e174b70c62160404464a7").then(res => console.log(res));
+
+// if the post exists, it will return something like
+/*
+{
+	md5: "00304f67656e174b70c62160404464a7",
+  	tags: [{
+		id: 2153,
+       	name: "presenting",
+       	count: 84694,
+       	type: 0,
+		type_locked: null
+	}]
+}*/
+// tags will be an array of tags, with each having the same structure
+// if the post does not exist, it will return null
+```
+
+`listPosts` function, you can use this almost the same as if you were searching on e621.net
+parameters (none are required):
+* `tags` - the tags to look for in posts (maximum of 6)
+* `limit` - the maximum number of posts per page (max 320)
+* `page` - the page to go to in the results (max 750)
+* `beforeId` - look for posts before this post id
+* `filterTags` - an array of tags to filter posts with, if a post contains any tags that are listed here, it will not be returned
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//           tags (max 6),           limit, page, beforeId, filterTags
+e6.listPosts(["gay", "order:favcount"], 50,    1,     null, []).then(res => console.log(res));
+// returns an array of posts matching the query, returns an empty array if none were found
+```
+
+this will return zero results, because all of them will have a tag that is filtered out
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+e6.listPosts(["gay"], null, null, null, ["male"]).then(res => console.log(res));
+// in the console: []
+```
+
+fetch deleted posts:
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+//              page, user id
+e6.getDeletedPosts(1, null).then(res => console.log(res));
+// will return an array of entries like
+/*
+{
+	id: number,
+	creator_id: number,
+	tags: string.
+	delreason: string
+}
+*/
+```
+
+get the most popular posts on e621
+```js
+const E6API = require("E6API");
+const e6 = new E6API({
+	userAgent: "E6API/1.0.2 (https://github.com/FurryBotCo/E6API)"
+});
+
+// valid values are "day", "week", and "month"
+e6.getPopularPosts("month").then(res => console.log(res));
+// will return an array of e621 posts
+```
